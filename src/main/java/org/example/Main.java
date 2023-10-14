d849a1454a8fd5390e7811e9fd245058f01b7a23
Spirit of energy <spiritofenergy220@gmail.com>
2023-10-14 13:24:47 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,32 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //  1. Где хранятся переменные? Сколько переменных можно задать в
+        // программе? Чем ограничен размер?
+        // Ответ: Переменные хранятся в оперативной памяти компьютера.
+        // Переменных может быть неограниченное колисество.
+        // Размер ограничен типом переменной, количеством байтов и логикой.
+
+        // 2. Пользователь вводит строку, выведите ее длину
+        Scanner scan =  new Scanner(System.in);
+        System.out.println("Введите слово или строку:");
+        String str1 = scan.nextLine();
+        System.out.println(str1.length() );
+
+        //  3. Пользователь вводит две строки, выведите сумму их длин
+
+        System.out.println(" Введите две строки");
+        String unoString = scan.nextLine();
+        System.out.println(unoString.length() + " букв");
+
+        System.out.println(" ... И вторую строку");
+        String twoString = scan.nextLine();
+        System.out.println(twoString.length() + " букв");
+
+        System.out.println(unoString.length() + twoString.length());
+
     }
 }
